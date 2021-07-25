f7fd7b7cf80c0597959e4e2f1ddc0d758877a736
Arif Shaikh <arif.injaz@outlook.com>
2021-07-25 09:01:56 +0530

    Simple Pyramid

---
+++ mario/mario_more.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int only_positive(void);
+
+int main(void)
+{
+    int n = only_positive(); // This shall capture positive results between 1 to 8
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int k = i + 1; k < n ; k++) // Loop to add spaces
+        {
+            printf(" ");
+        }
+
+        for (int j = 0; j <= i; j++) //Loop to add #s
+        {
+
+            printf("#");
+        }
+
+        for (int k = 0; k <= 0 ; k++) // Loop to add 1 spaces
+        {
+            printf("  ");
+        }
+
+        for (int j = 0; j <= i; j++) //Loop to add #s
+        {
+
+            printf("#");
+        }
+
+        printf("\n");
+    }
+
+}
+
+int only_positive(void)
+{
+    int v;
+    do
+    {
+        v = get_int("Height\n");  //Input for the height
+
+    }
+    while (v < 1 || v > 8); // condition to get results between 1 to 8
+    return v;
+}
